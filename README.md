a0a351c7da7a997c6fb33c3ac8e87abb7ef48230
BrrunaRibeiro <brrunarib@gmail.com>
2023-10-31 19:23:53 +0100

    Add PasswordManager class and main function

---
+++ README.md
@@ -0,0 +1,5 @@
+## Libraries 
+
+* Gspred
+* Google-Auth(Credentials class only)
+
--- run.py
+++ run.py
@@ -1,13 +1,32 @@
+#Imports the Gspred library to access all of it's  functions, class or methods within it.
 import gspread
-from oauth2client.service_account import Credentials
+
+#Imports Credentials class(part of the service_account function from the Google Auth library).
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets"
-    "https://www.googleapis.com/auth/drive.file"
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-]
+    ]
 
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.wtih_scopes(SCOPE)
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("password_manager")
+SHEET = GSPREAD_CLIENT.open("password-manager-file")
+
+class PasswordManager:
+    """
+    Defines main Class to run the program
+    """
+    def __init__(self, sheet):
+        self.sheet = sheet
+
+
+def main():
+    """
+    Calls the main Class with the SHEET variable as a argument.
+    """
+    manager = PasswordManager(SHEET)
+
+main()
