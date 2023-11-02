073018606d0f4b7898713f665bb405344d1ce239
BrrunaRibeiro <brrunarib@gmail.com>
2023-11-02 19:27:27 +0100

    Fix errors in main functions

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 * Gspred
 * Google-Auth(Credentials class only)
 
+## Fixed bugs
+* The gspread' update() method was the first option. However it genereated the error:
+``` UserWarning: [Deprecated][in version 6.0.0]: method signature will change to: 'Worksheet.update(value = [[]], range_name=)' arguments 'range_name' and 'values' will swap, values will be mandatory of type: 'list(list(...))'
+  warnings.warn( ```
+ I seached for a solution both online and with Code Institute Tutors, they advised to look for an alternative code as there is an issue with the method signature from certain versions of the gspread library.
+ As an alternative 
--- run.py
+++ run.py
@@ -13,7 +13,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("password-manager-file")
+SHEET = GSPREAD_CLIENT.open("password-manager-file").sheet1
 
 class PasswordManager:
     """
@@ -41,39 +41,47 @@ class PasswordManager:
         
         """
         try:
-            self.sheet.append_table([account_name, username, password])
+            data = [[account_name, username, password]]
+            self.sheet.append_rows(data)
             print(f"{account_name}'s account added sucessfully.")
         except Exception as e:
             print(f"Error {e}") #double check if it works
-    
+
     def view_specific_account(self, account_name):
         """
-        
+        Iterate's through the list of dictionaries returned by get_all_records().
+        Finds the account name and prints the dictionary containg the account details.
+        Prints an
         """
         try:
-            data = self.sheet.get_all_values()
+            data = self.sheet.get_all_records()
+            account_name = account_name.strip()
             for account in data:
-                if account['account_name'] == account_name:
+                if 'Account Name' in account and account["Account Name"].strip() == account_name:
                     print(account)
                     return
             print(f"{account_name}'s account not found.")
         except Exception as e:
             print(f"Error {e}") #double check if it works
-    
+
     def update_account(self, account_name, new_username, new_password):
         """
+        Iterate's through the list of dictionaries returned by get_all_records().
+        The strip() method is used to help with typos and possible whitespaces issues.
+        First the Account is deleted and then added again with the new_username and new_password.
         
         """
         try:
-            data = self.sheet.get_all_values()
+            data = self.sheet.get_all_records()
+            #account_name = account_name.strip()
             for account in data:
                 if account['Account Name'] == account_name:
-                    account['Username'] = new_username
-                    account['Password'] = new_password
                     self.sheet.delete_rows(data.index(account) + 2)
-                    self.sheet.insert_row([account_name, new_username, new_password], data.index(account) + 2)
-                    print(f"{account_name}'s account updated successfully.")
-                    return
+
+            data = [[account_name, new_username, new_password]]
+            self.sheet.append_rows(data)
+            print(f"{account_name}'s account updated successfully.")
+            return
             print(f"{account_name}'s account not found.")
         except Exception as e:
             print(f"Error: {e}") #double check if it works
@@ -83,7 +91,8 @@ class PasswordManager:
         
         """
         try:
-            data = self.sheet.get_all_values()
+            data = self.sheet.get_all_records()
+            #account_name = account_name.strip()
             for account in data:
                 if account['Account Name'] == account_name:
                     self.sheet.delete_rows(data.index(account) + 2)
@@ -106,7 +115,8 @@ def main():
     Validation for empty or incorrect input.
     """
     manager = PasswordManager(SHEET)
-    
+    print("Welcome to Password Manager")
+
     while True:
         print("\nPassword Manager Options: ")
         print("\n1. View all saved passwords")
@@ -118,28 +128,32 @@ def main():
 
         choice = input("\nEnter your choice(Ex:'2'): ")
 
-        if choice == '1':
-            manager.view_all_accounts()
-        elif choice == '2':
-            account_name = input("Enter the Account Name(Ex:'Netflix'): ")
-            username = input("Enter the Username: ")
-            password = input("ENter the Password: ")
-            manager.add_account(account_name, username, password)
-        elif choice == '3':
-            account_name = input("Enter the Account Name to view(Ex:'Netflix'): ")
-            manager.view_specific_account(account_name)
-        elif choice == '4':
-            account_name = input("Enter the Account Name to update:")
-            new_username = input("Enter your new username: ")
-            new_password = input("Enter your new password: ")
-            manager.update_account(account_name, new_username, new_password)
-        elif choice == '5':
-            account_name = input("Enter the Account Name you wish to delete: ")
-            manager.delete_account(account_name)
-        elif choice == '6':
-            manager.leave_application()
-            break
-        else:
-            print("Invalid choice. Please enter a valid option.")
-
+        try:
+            if choice == '1':
+                manager.view_all_accounts()
+            elif choice == '2':
+                account_name = input("Enter the Account Name(Ex:'Netflix'): ")
+                username = input("Enter the Username: ")
+                password = input("Enter the Password: ")
+                manager.add_account(account_name, username, password)
+            elif choice == '3':
+                account_name = input("Enter the Account Name to view(Ex:'Netflix'): ")
+                manager.view_specific_account(account_name)
+            elif choice == '4':
+                account_name = input("Enter the Account Name to update: ")
+                new_username = input("Enter your new username: ")
+                new_password = input("Enter your new password: ")
+                manager.update_account(account_name, new_username, new_password)
+            elif choice == '5':
+                account_name = input("Enter the Account Name you wish to delete: ")
+                manager.delete_account(account_name)
+            elif choice == '6':
+                manager.leave_application()
+                break
+            else:
+                raise ValueError(f"Valid Options are '1', '2', '3', '4', '5' and '6'. You provided '{choice}'")
+        except ValueError as e:
+            print(f"Invalid choice: {e}. Please enter a valid option.")
+            return False
+            #Must ask options again..
 main()
